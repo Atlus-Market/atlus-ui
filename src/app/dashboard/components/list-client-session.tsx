02675f11211063c41c6fae3462833ed9e20d02b4
Sergio Milici <sergio.milici@gmail.com>
2023-07-13 11:00:24 -0300

    Setup redux toolkit

--- src/app/dashboard/components/list-client-session.tsx
+++ src/app/dashboard/components/list-client-session.tsx
@@ -1,19 +1,15 @@
 'use client';
 
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { AtlusButton } from '@/components/ui/button/atlus-button';
 import { LoginRoute } from '@/constants/routes';
 
 export const ListClientSession = () => {
-  const session = useSession();
-  console.log(session);
   return (
     <div>
-      Client Session
-
       <AtlusButton onClick={() => signOut({
         callbackUrl: LoginRoute,
-        redirect: true,
+        redirect: true
       })}>Logout</AtlusButton>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,24 @@ import { Inter } from 'next/font/google';
 import { ApiClientProvider } from '@/api/api-client-provider';
 import { ReactNode } from 'react';
 import { AtlusSessionProvider } from '@/app/(auth)/session/atlus-session-provider';
+import { StoreProvider } from '@/redux/store-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Atlus',
-  description: '',
+  description: ''
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
     <body className={inter.className}>
-    <AtlusSessionProvider>
-      <ApiClientProvider>{children}</ApiClientProvider>
-    </AtlusSessionProvider>
+    <StoreProvider>
+      <AtlusSessionProvider>
+        <ApiClientProvider>{children}</ApiClientProvider>
+      </AtlusSessionProvider>
+    </StoreProvider>
     </body>
     </html>
   );
