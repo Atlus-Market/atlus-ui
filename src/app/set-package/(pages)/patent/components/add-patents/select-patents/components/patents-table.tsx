89d22db69afb20192291f9a4e8a31404f96c5f1d
Sergio Milici <sergio.milici@gmail.com>
2023-07-19 12:00:32 -0300

    wip: person test

--- src/app/set-package/(pages)/patent/components/add-patents/select-patents/components/patents-table.tsx
+++ src/app/set-package/(pages)/patent/components/add-patents/select-patents/components/patents-table.tsx
@@ -2,16 +2,17 @@ import {
   patentsMock
 } from '@/app/set-package/(pages)/patent/components/add-patents/enter-patents-manually/tabs/enter-patents-number/get-patents-mock';
 import {
+  Column,
   ColumnDef,
-  createColumnHelper,
+  createColumnHelper, ExpandedState,
   flexRender,
   getCoreRowModel,
-  getExpandedRowModel,
-  getGroupedRowModel,
-  GroupingState,
+  getExpandedRowModel, getFilteredRowModel,
+  getGroupedRowModel, getPaginationRowModel,
+  GroupingState, Table,
   useReactTable
 } from '@tanstack/react-table';
-import { useState } from 'react';
+import { HTMLProps, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 
 import './styles.css';
 import {
@@ -23,6 +24,10 @@ import {
 } from '@/app/set-package/(pages)/patent/components/add-patents/select-patents/components/table/row-cell';
 import { AtlusTag } from '@/components/ui/tag/atlus-tag';
 import format from 'date-fns/format';
+import {
+  makeData,
+  Person
+} from '@/app/set-package/(pages)/patent/components/add-patents/select-patents/components/make-data';
 
 type Patent = {
   publicationNumber: string; // also patent id
@@ -102,73 +107,327 @@ const columns: ColumnDef<Patent, any>[] = [
 ];
 
 export const PatentsTable = () => {
-  console.log('---------------------------------------------');
-  const [data, setData] = useState(() => [...defaultData]);
-  const [grouping, setGrouping] = useState<GroupingState>([]);
+  const rerender = useReducer(() => ({}), {})[1]
+
+  const columns = useMemo<ColumnDef<Person>[]>(
+    () => [
+      {
+        header: 'Name',
+        footer: props => props.column.id,
+        columns: [
+          {
+            accessorKey: 'firstName',
+            header: ({ table }) => (
+              <>
+                <IndeterminateCheckbox
+                  {...{
+                    checked: table.getIsAllRowsSelected(),
+                    indeterminate: table.getIsSomeRowsSelected(),
+                    onChange: table.getToggleAllRowsSelectedHandler(),
+                  }}
+                />{' '}
+                <button
+                  {...{
+                    onClick: table.getToggleAllRowsExpandedHandler(),
+                  }}
+                >
+                  {table.getIsAllRowsExpanded() ? '👇' : '👉'}
+                </button>{' '}
+                First Name
+              </>
+            ),
+            cell: ({ row, getValue }) => (
+              <div
+                style={{
+                  // Since rows are flattened by default,
+                  // we can use the row.depth property
+                  // and paddingLeft to visually indicate the depth
+                  // of the row
+                  paddingLeft: `${row.depth * 2}rem`,
+                }}
+              >
+                <>
+                  <IndeterminateCheckbox
+                    {...{
+                      checked: row.getIsSelected(),
+                      indeterminate: row.getIsSomeSelected(),
+                      onChange: row.getToggleSelectedHandler(),
+                    }}
+                  />{' '}
+                  {row.getCanExpand() ? (
+                    <button
+                      {...{
+                        onClick: row.getToggleExpandedHandler(),
+                        style: { cursor: 'pointer' },
+                      }}
+                    >
+                      {row.getIsExpanded() ? '👇' : '👉'}
+                    </button>
+                  ) : (
+                    '🔵'
+                  )}{' '}
+                  {getValue()}
+                </>
+              </div>
+            ),
+            footer: props => props.column.id,
+          },
+          {
+            accessorFn: row => row.lastName,
+            id: 'lastName',
+            cell: info => info.getValue(),
+            header: () => <span>Last Name</span>,
+            footer: props => props.column.id,
+          },
+        ],
+      },
+      {
+        header: 'Info',
+        footer: props => props.column.id,
+        columns: [
+          {
+            accessorKey: 'age',
+            header: () => 'Age',
+            footer: props => props.column.id,
+          },
+          {
+            header: 'More Info',
+            columns: [
+              {
+                accessorKey: 'visits',
+                header: () => <span>Visits</span>,
+                footer: props => props.column.id,
+              },
+              {
+                accessorKey: 'status',
+                header: 'Status',
+                footer: props => props.column.id,
+              },
+              {
+                accessorKey: 'progress',
+                header: 'Profile Progress',
+                footer: props => props.column.id,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const [data, setData] = useState(() => makeData(100, 5, 3))
+  const refreshData = () => setData(() => makeData(100, 5, 3))
+
+  const [expanded, setExpanded] = useState<ExpandedState>({})
 
   const table = useReactTable({
     data,
     columns,
     state: {
-      grouping
+      expanded,
     },
-    onGroupingChange: setGrouping,
-    getExpandedRowModel: getExpandedRowModel(),
-    getGroupedRowModel: getGroupedRowModel(),
+    onExpandedChange: setExpanded,
+    getSubRows: row => row.subRows,
     getCoreRowModel: getCoreRowModel(),
-    debugTable: true
-  });
-  console.log('groupingState: ', grouping);
+    getPaginationRowModel: getPaginationRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    debugTable: true,
+  })
 
   return (
-    <div className='p-2'>
+    <div className="p-2">
+      <div className="h-2" />
       <table>
-        <thead className='text-left whitespace-nowrap'>
-          {table.getHeaderGroups().map(headerGroup => {
-            console.log('****** headerGroup: ', headerGroup);
-            return (
-              <tr key={headerGroup.id}  {...headerGroup}>
-                {headerGroup.headers.map(header => {
-                    console.log(`headerId: ${header.id}: canGroup`, header.column.getCanGroup());
-                    return (
-                      <th key={header.id} className='px-4'>
-                        {header.isPlaceholder
-                          ? null
-                          : headerGroup.depth === 0 ?
-                            <div>
-                              <button onClick={header.column.getToggleGroupingHandler()}>
-                                {header.column.getIsGrouped() ? 'Grouped' : 'Group'}
-                              </button>
-                              {flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                            </div>
-                            : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </th>
-                    );
-                  }
-                )}
-              </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map(row => (
-            <tr key={row.id} className='bg-white'>
-              {<>{console.log(row.getVisibleCells())}</>}
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id} className='pt-5 pb-8 px-4'>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <Filter column={header.column} table={table} />
+                          </div>
+                        ) : null}
+                      </div>
+                    )}
+                  </th>
+                )
+              })}
             </tr>
           ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </table>
+      <div className="h-2" />
+      <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>{table.getRowModel().rows.length} Rows</div>
+      <div>
+        <button onClick={() => rerender()}>Force Rerender</button>
+      </div>
+      <div>
+        <button onClick={() => refreshData()}>Refresh Data</button>
+      </div>
+      <pre>{JSON.stringify(expanded, null, 2)}</pre>
     </div>
-  );
-};
+  )
+}
+
+function Filter({
+                  column,
+                  table,
+                }: {
+  column: Column<any, any>
+  table: Table<any>
+}) {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className="flex space-x-2">
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className="w-24 border shadow rounded"
+      />
+      <input
+        type="number"
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className="w-24 border shadow rounded"
+      />
+    </div>
+  ) : (
+    <input
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+      onChange={e => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded"
+    />
+  )
+}
+
+function IndeterminateCheckbox({
+                                 indeterminate,
+                                 className = '',
+                                 ...rest
+                               }: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+  const ref = useRef<HTMLInputElement>(null!)
+
+  useEffect(() => {
+    if (typeof indeterminate === 'boolean') {
+      ref.current.indeterminate = !rest.checked && indeterminate
+    }
+  }, [ref, indeterminate])
+
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={className + ' cursor-pointer'}
+      {...rest}
+    />
+  )
+}
