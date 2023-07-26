9e5cca92ed67107bd37f9fdd7fc4be5653fbb13f
Sergio Milici <sergio.milici@gmail.com>
2023-07-26 18:27:25 -0300

    wip: Implemented dropdown list

--- src/app/set-package/(pages)/patent/page.tsx
+++ src/app/set-package/(pages)/patent/page.tsx
@@ -1,11 +1,15 @@
-import { AtlusDropdownList } from '@/components/ui/select/atlus-dropdown-list';
+import {
+  AddPatentsModal
+} from '@/app/set-package/(pages)/patent/components/add-patents/add-patents-modal';
+import {
+  PatentsFamilyList
+} from '@/app/set-package/(pages)/patent/components/patents-family-list/patents-family-list';
 
 export default async function PatentPage() {
   return (
     <div>
-      <AtlusDropdownList placeholder='Colors' />
-      {/*<PatentsFamilyList />*/}
-      {/*<AddPatentsModal />*/}
+      <PatentsFamilyList />
+      <AddPatentsModal />
     </div>
   );
 }
--- src/components/ui/select/atlus-dropdown-list.tsx
+++ src/components/ui/select/atlus-dropdown-list.tsx
@@ -1,111 +1,59 @@
 'use client';
 
-import Select, { StylesConfig } from 'react-select';
-import chroma from 'chroma-js';
+import Select from 'react-select';
 import clsx from 'clsx';
+import { ReactNode } from 'react';
 
-export interface ColourOption {
+export interface DropdownOption {
   readonly value: string;
-  readonly label: string;
-  readonly color: string;
-  readonly isFixed?: boolean;
-  readonly isDisabled?: boolean;
+  readonly label: ReactNode;
+  options?: Omit<DropdownOption, 'options'>[];
 }
 
-export const colourOptions: readonly ColourOption[] = [
-  { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
-  { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
-  { value: 'purple', label: 'Purple', color: '#5243AA' },
-  { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
-  { value: 'orange', label: 'Orange', color: '#FF8B00' },
-  { value: 'yellow', label: 'Yellow', color: '#FFC400' },
-  { value: 'green', label: 'Green', color: '#36B37E' },
-  { value: 'forest', label: 'Forest', color: '#00875A' },
-  { value: 'slate', label: 'Slate', color: '#253858' },
-  { value: 'silver', label: 'Silver', color: '#666666' }
-];
-
-const dot = (color = 'transparent') => ({
-  alignItems: 'center',
-  display: 'flex',
-
-  ':before': {
-    backgroundColor: color,
-    borderRadius: 10,
-    content: '" "',
-    display: 'block',
-    marginRight: 8,
-    height: 10,
-    width: 10
+const classNames = {
+  container: () => 'rounded-lg',
+  control: () => clsx(
+    'px-4 py-[16px] m-0',
+    'rounded-lg border border-solid border-lightest-grey'
+  ),
+  valueContainer: () => 'text-soft-black text-sm font-normal leading-[15px]',
+  placeholder: () => 'bg-white text-xs font-medium text-middle-grey leading-[15px]',
+  input: () => 'text-xs font-medium text-soft-black leading-[15px]',
+  menu: () => clsx(
+    'rounded-lg border border-solid border-lightest-grey py-[10px]',
+    'shadow-[0px_2px_6px_0px_rgba(164,162,160,0.25)]',
+    'mt-[13px]'
+  ),
+  groupHeading: () => clsx(
+    'text-middle-grey text-xs font-medium',
+    'px-[20px] py-[10px]'
+  ),
+  option: () => {
+    return clsx(
+      'px-[20px] py-[10px]',
+      'text-soft-black !text-sm font-medium !leading-[17px]',
+      'hover:bg-lightest-grey'
+    );
   }
-});
-
-const colourStyles: StylesConfig<ColourOption> = {
-  control: (styles) => ({ ...styles, backgroundColor: 'white' }),
-  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-    const color = chroma(data.color);
-    return {
-      ...styles,
-      backgroundColor: isDisabled
-        ? undefined
-        : isSelected
-          ? data.color
-          : isFocused
-            ? color.alpha(0.1).css()
-            : undefined,
-      color: isDisabled
-        ? '#ccc'
-        : isSelected
-          ? chroma.contrast(color, 'white') > 2
-            ? 'white'
-            : 'black'
-          : data.color,
-      cursor: isDisabled ? 'not-allowed' : 'default',
-
-      ':active': {
-        ...styles[':active'],
-        backgroundColor: !isDisabled
-          ? isSelected
-            ? data.color
-            : color.alpha(0.3).css()
-          : undefined
-      }
-    };
-  },
-  input: (styles) => ({ ...styles, ...dot() }),
-  placeholder: (styles) => ({ ...styles }),
-  singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) })
 };
 
 interface AtlusDropdownListProps {
   placeholder?: string;
+  isOpen?: boolean;
+  options: DropdownOption[];
+  defaultValue?: DropdownOption;
 }
 
-export const AtlusDropdownList = ({ placeholder }: AtlusDropdownListProps) => {
-
+export const AtlusDropdownList = ({ isOpen, placeholder, options = [], defaultValue }: AtlusDropdownListProps) => {
   return (
-    <div>
-      <Select
-        menuIsOpen={true}
-        unstyled={true}
-        isMulti={false}
-        // defaultValue={colourOptions[2]}
-        options={colourOptions}
-        placeholder={placeholder}
-        classNames={{
-          control: () => 'px-0 py-0 m-0',
-          valueContainer: () => 'px-[20px] py-[10px]',
-          placeholder: () => 'bg-white text-xs font-medium text-middle-grey',
-          option: (props) => {
-            return clsx(
-              'px-[20px] py-[10px]',
-              'text-soft-black !text-sm font-medium !leading-[17px]',
-              'hover:bg-lightest-grey'
-            );
-          }
-        }
-        }
-      />
-    </div>
+    <Select
+      menuIsOpen={isOpen}
+      unstyled={true}
+      isMulti={false}
+      defaultValue={defaultValue}
+      options={options}
+      placeholder={placeholder}
+      classNames={classNames}
+    />
   );
 };
