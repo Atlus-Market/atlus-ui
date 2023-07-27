3c6423bc0cfc1ad8cf91da4e09a9906f3adbc302
Sergio Milici <sergio.milici@gmail.com>
2023-07-27 15:50:39 -0300

    wip: Refactor input

--- src/components/ui/form/atlus-form-input-with-tags.tsx
+++ src/components/ui/form/atlus-form-input-with-tags.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { AtlusInputWithTagsProps } from '@/components/ui/input/atlus-input-with-tags';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import {
+  AtlusInputWithTags,
+  AtlusInputWithTagsProps
+} from '@/components/ui/input/atlus-input-with-tags';
 import { Controller, useFormContext } from 'react-hook-form';
-import { AtlusInput } from '@/components/ui/input/atlus-input';
-import { AtlusTag } from '@/components/ui/tag/atlus-tag';
-import { isArray } from 'lodash';
+import { useCallback } from 'react';
 
 export interface AtlusFormInputWithTagsProps extends AtlusInputWithTagsProps {
   name: string;
@@ -13,61 +13,18 @@ export interface AtlusFormInputWithTagsProps extends AtlusInputWithTagsProps {
 
 export const AtlusFormInputWithTags = (props: AtlusFormInputWithTagsProps) => {
   const { name } = props;
-  const [tags, setTags] = useState<string[]>([]);
   const { control, setValue } = useFormContext();
-  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      event.stopPropagation();
-      const value = (event.target as HTMLInputElement).value;
-      if (value?.trim().length > 0) {
-        if (inputRef.current) {
-          inputRef.current.value = '';
-        }
-        setTags([
-          ...tags,
-          value
-        ]);
-      }
-    }
-  };
-
-  useEffect(() => {
+  const onTagsChange = useCallback((tags: string[]) => {
     setValue(name, tags);
-  }, [tags, name, setValue]);
-
-  const tagsList = useMemo(() => {
-    if (!isArray(tags) || tags.length === 0) {
-      return null;
-    }
-    return (
-      <div className='flex items-center flex-wrap gap-2 mr-2'>
-        {tags.map((tag, index) => {
-          const onClose = () => {
-            setTags(tags.filter(t => t !== tag));
-          };
-          return (
-            <AtlusTag key={`${index}-${tag}`} text={tag} onClose={onClose} />
-          );
-        })}
-      </div>
-    );
-  }, [tags]);
+  }, [name, setValue]);
 
   return (
     <Controller
       name={name}
       control={control}
       render={() => (
-        <AtlusInput
-          {...props}
-          ref={inputRef}
-          leftCmp={tagsList}
-          onKeyDown={handleKeyDown}
-          inputClassName='min-w-[310px]'
-        />
+        <AtlusInputWithTags{...props} onTagsChange={onTagsChange} />
       )}
     />
   );
--- src/components/ui/input/atlus-input-with-tags.tsx
+++ src/components/ui/input/atlus-input-with-tags.tsx
@@ -1,9 +1,64 @@
 import { AtlusInput, AtlusInputProps } from '@/components/ui/input/atlus-input';
+import { ForwardedRef, forwardRef, useEffect, useMemo, useRef, useState } from 'react';
+import { isArray } from 'lodash';
+import { AtlusTag } from '@/components/ui/tag/atlus-tag';
 
-export type AtlusInputWithTagsProps = AtlusInputProps;
+export interface AtlusInputWithTagsProps extends AtlusInputProps {
+  onTagsChange?: (tags: string[]) => void;
+}
 
-export const AtlusInputWithTags = (props: AtlusInputWithTagsProps) => {
-  return (
-    <AtlusInput {...props} />
-  );
-};
+export const AtlusInputWithTags = forwardRef<HTMLInputElement, AtlusInputWithTagsProps>(
+  function AtlusInputWithTags({ onTagsChange, name, ...rest }, ref: ForwardedRef<HTMLInputElement>) {
+    const [tags, setTags] = useState<string[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      onTagsChange?.(tags);
+    }, [tags, name, onTagsChange]);
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        event.stopPropagation();
+        const value = (event.target as HTMLInputElement).value;
+        if (value?.trim().length > 0) {
+          if (inputRef.current) {
+            inputRef.current.value = '';
+          }
+          setTags([
+            ...tags,
+            value
+          ]);
+        }
+      }
+    };
+
+    const tagsList = useMemo(() => {
+      if (!isArray(tags) || tags.length === 0) {
+        return null;
+      }
+      return (
+        <div className='flex items-center flex-wrap gap-2 mr-2'>
+          {tags.map((tag, index) => {
+            const onClose = () => {
+              setTags(tags.filter(t => t !== tag));
+            };
+            return (
+              <AtlusTag key={`${index}-${tag}`} text={tag} onClose={onClose} />
+            );
+          })}
+        </div>
+      );
+    }, [tags]);
+
+    return (
+      <AtlusInput
+        {...rest}
+        name={name}
+        ref={inputRef}
+        leftCmp={tagsList}
+        onKeyDown={handleKeyDown}
+        inputClassName='min-w-[310px]'
+      />
+    );
+  });
