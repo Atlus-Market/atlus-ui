ae9f35f2cee5670298b93432a355b9d90c6331f4
Sergio Milici <sergio.milici@gmail.com>
2023-07-28 17:01:54 -0300

    wip: disable add to package button if no patents selected

--- src/app/set-package/(pages)/patent/components/add-patents/select-patents/select-patents-next-button.tsx
+++ src/app/set-package/(pages)/patent/components/add-patents/select-patents/select-patents-next-button.tsx
@@ -3,14 +3,25 @@
 import { AtlusButton } from '@/components/ui/button/atlus-button';
 import { useAppDispatch } from '@/redux/hooks';
 import { hideAddPatentsModal, setPackagePatents } from '@/redux/features/set-package/set-package';
+import {
+  useFamilyPatentsHelper
+} from '@/app/set-package/(pages)/patent/hooks/use-family-patents-helper';
+import { useSelector } from 'react-redux';
+import {
+  selectSelectedFamilyPatents
+} from '@/redux/features/set-package/selectors/add-patents-selectors';
 
 export const SelectPatentsNextButton = () => {
   const dispatch = useAppDispatch();
+  const selectedFamilyPatents = useSelector(selectSelectedFamilyPatents);
+  const { patentsCount } = useFamilyPatentsHelper(selectedFamilyPatents);
   return (
-    <AtlusButton onClick={() => {
-      dispatch(setPackagePatents());
-      dispatch(hideAddPatentsModal());
-    }}>
+    <AtlusButton
+      disabled={patentsCount === 0}
+      onClick={() => {
+        dispatch(setPackagePatents());
+        dispatch(hideAddPatentsModal());
+      }}>
       Add to package
     </AtlusButton>
   );
