ea6112c5712b5751d0d373a19f70b935d67ab849
Brian Hoffman <hoffmabc@users.noreply.github.com>
2023-08-09 11:03:14 -0400

    Deprecate port in public api endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,15 +2,14 @@ import axios, { AxiosResponse, Method } from 'axios';
 import { AtlusSessionManager } from '@/app/(auth)/session/atlus-session-manager';
 import { Session } from 'next-auth';
 
-
 export const createUrl = (endpoint: string): string => {
   const cleanEndpoint = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
-  return `${process.env.NEXT_PUBLIC_API_ENDPOINT}:${process.env.NEXT_PUBLIC_API_ENDPOINT_PORT}${cleanEndpoint}`;
+  return `${process.env.NEXT_PUBLIC_API_ENDPOINT}${cleanEndpoint}`;
 };
 
 export enum ProtectedEndpoint {
   True = 1,
-  False = 0
+  False = 0,
 }
 
 interface AuthHeadersProvider {
@@ -18,14 +17,19 @@ interface AuthHeadersProvider {
   csrfToken: string | undefined;
 }
 
-export const mapServerSessionToAuthHeadersProvider = (serverSession: Session): AuthHeadersProvider => {
+export const mapServerSessionToAuthHeadersProvider = (
+  serverSession: Session
+): AuthHeadersProvider => {
   return {
     accessToken: serverSession.user?.accessToken,
-    csrfToken: serverSession.user?.csrfToken
+    csrfToken: serverSession.user?.csrfToken,
   };
 };
 
-export const setAuthHeaders = (headers: Record<string, string>, provider: AuthHeadersProvider): Record<string, string> => {
+export const setAuthHeaders = (
+  headers: Record<string, string>,
+  provider: AuthHeadersProvider
+): Record<string, string> => {
   headers['Authorization'] = `Bearer ${provider.accessToken}`;
   headers['X-CSRF-TOKEN'] = provider.csrfToken ?? '';
   return headers;
@@ -37,7 +41,6 @@ export const createRequest = <Payload, Response>(
   isProtected: ProtectedEndpoint = ProtectedEndpoint.False,
   payload?: Payload | undefined
 ): Promise<Response> => {
-
   const headers = {};
   if (isProtected === ProtectedEndpoint.True) {
     setAuthHeaders(headers, AtlusSessionManager);
@@ -48,7 +51,7 @@ export const createRequest = <Payload, Response>(
     url: createUrl(endpoint),
     headers,
     data: payload,
-    withCredentials: isProtected === ProtectedEndpoint.True
+    withCredentials: isProtected === ProtectedEndpoint.True,
   }).then(response => {
     return response.data;
   });
