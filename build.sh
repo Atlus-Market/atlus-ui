f432304836138d6cdf0fd600c75c863d43c1088c
Brian Hoffman <hoffmabc@users.noreply.github.com>
2023-08-11 15:04:25 -0400

    Clean up build scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Please provide both the tag name and the repository name as parameters."
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+  echo "Please provide the tag name, repository name, and public API endpoint as parameters."
   exit 1
 fi
 
 version="$1"
 repository="$2"
+public_api_endpoint="$3"
+logrocket_token="$4"
 
 # Set the repository prefix based on the chosen repository
 if [ "$repository" == "docker" ]; then
@@ -20,11 +22,10 @@ else
   exit 1
 fi
 
-# Determine the local machine's IP address
-LOCAL_IP=$(ifconfig | grep 'inet ' | awk '{print $2}' | grep -E '^192\.168\.')
-
 # Build Docker image and pass the local IP as a build argument
-docker buildx build --platform linux/arm64 --build-arg LOCAL_IP=${LOCAL_IP} --progress=plain -t "$repo_prefix:$version" .
+docker buildx build --no-cache --platform linux/arm64 \
+  --build-arg NEXT_PUBLIC_LOG_ROCKET_TOKEN="$logrocket_token" \
+  --build-arg NEXT_PUBLIC_API_ENDPOINT="$public_api_endpoint" --progress=plain -t "$repo_prefix:$version" .
 
 # Push Docker image
 docker push "$repo_prefix:$version"
