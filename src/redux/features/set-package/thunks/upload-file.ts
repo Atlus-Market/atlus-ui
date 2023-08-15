1570a9bec44d455f29b570ada71a1f6a150f7c97
Sergio Milici <sergio.milici@gmail.com>
2023-08-15 18:36:24 -0300

    wip: cancel request

--- src/redux/features/set-package/thunks/upload-file.ts
+++ src/redux/features/set-package/thunks/upload-file.ts
@@ -3,9 +3,15 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export const uploadPackageFile = createAsyncThunk(
   'package/documents/uploadFile',
   async (userId, thunkAPI) => {
-    await fetch('https://hub.dummyapis.com/delay?seconds=3600', {
-      signal: thunkAPI.signal
-    });
+    try {
+      await fetch('https://hub.dummyapis.com/delay?seconds=3600', {
+        signal: thunkAPI.signal
+      });
+    } catch (e) {
+      console.error(e);
+      console.log('thunkAPI.signal: ', thunkAPI.signal);
+    }
+
     return { result: 123 };
   }
 );
