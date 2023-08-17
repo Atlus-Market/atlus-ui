00b6f6b43046640b1bf6a4e4bc9f6c277f830e5b
Sergio Milici <sergio.milici@gmail.com>
2023-08-17 17:06:53 -0300

    wip: uploading file state

--- src/redux/features/set-package/slices/documents.ts
+++ src/redux/features/set-package/slices/documents.ts
@@ -5,8 +5,7 @@ import { uploadPackageDocument } from '@/redux/features/set-package/thunks/uploa
 import { ActionReducerMapBuilder } from '@reduxjs/toolkit/src/mapBuilders';
 import { cleanSerializedFile } from '@/utils/file';
 
-
-export interface SerializedFile {
+export interface SerializedFileUpload {
   id: string;
   name: string;
   size: number;
@@ -29,7 +28,7 @@ export interface DocumentsState {
 export const documentsInitialState: DocumentsState = {
   dataroom: undefined,
   uploadFilesQueue: [],
-  uploadingFiles: {}
+  uploadingFiles: {},
 };
 
 export const documentsReducer = {
@@ -38,75 +37,77 @@ export const documentsReducer = {
   },
 
   // File Upload
-  addFileToUpload: (state: SetPackageState, action: PayloadAction<SerializedFile>) => {
+  addFileToUpload: (
+    state: SetPackageState,
+    action: PayloadAction<SerializedFileUpload>
+  ) => {
     state.documents.uploadFilesQueue.push(action.payload);
   },
-  updateFileUploadState: (state: SetPackageState, action: PayloadAction<UploadingFileState>) => {
+  updateFileUploadState: (
+    state: SetPackageState,
+    action: PayloadAction<UploadingFileState>
+  ) => {
     if (!state.documents.uploadingFiles[action.payload.requestId]) {
       return;
     }
     state.documents.uploadingFiles[action.payload.requestId] = action.payload;
   },
-  removeQueuedFile: (state: SetPackageState, action: PayloadAction<{ fileId: string }>) => {
-    const serializedFile = state.documents.uploadFilesQueue.find(file => file.id === action.payload.fileId);
-    if (serializedFile) {
-      cleanSerializedFile(serializedFile);
-      state.documents.uploadFilesQueue = state.documents.uploadFilesQueue.filter(file => file.id !== action.payload.fileId);
-    }
-  },
-  toggleDocumentVisibility: (state: SetPackageState, action: PayloadAction<{
-    documentId: string
-  }>) => {
-    const dataroom = state.documents.dataroom;
-    if (!dataroom) {
-      return;
-    }
-    const directoryTree = getDirectoryTree(dataroom.directoryTree, action.payload.documentId);
-    if (directoryTree) {
-      directoryTree.private = !directoryTree.private;
-    }
-  },
-  removeDocument: (state: SetPackageState, action: PayloadAction<{ documentId: string }>) => {
-    if (!state.documents.dataroom) {
-      return;
-    }
-    removeDirectoryTree(state.documents.dataroom.directoryTree, action.payload.documentId);
-  }
 };
 
-export const createDocumentsExtraReducers = (builder: ActionReducerMapBuilder<SetPackageState>) => {
-  builder.addCase(uploadPackageDocument.pending, (state: SetPackageState, action) => {
-    const { meta } = action;
-    const payload = meta.arg;
-    console.log('uploadPackageFile.pending:action ', action);
-    state.documents.uploadFilesQueue = state.documents.uploadFilesQueue.filter(serializedFile => serializedFile.id !== payload.id);
+export const createDocumentsExtraReducers = (
+  builder: ActionReducerMapBuilder<SetPackageState>
+) => {
+  builder.addCase(
+    uploadPackageDocument.pending,
+    (state: SetPackageState, action) => {
+      const { meta } = action;
+      const payload = meta.arg;
+      console.log('uploadPackageFile.pending:action ', action);
+      state.documents.uploadFilesQueue =
+        state.documents.uploadFilesQueue.filter(
+          serializedFile => serializedFile.id !== payload.id
+        );
 
-    state.documents.uploadingFiles[meta.requestId] = {
-      progress: 0,
-      requestId: meta.requestId,
-      serializedFile: payload
-    };
-  });
+      state.documents.uploadingFiles[meta.requestId] = {
+        progress: 0,
+        requestId: meta.requestId,
+        serializedFile: payload,
+      };
+    }
+  );
 
   // Add reducers for additional action types here, and handle loading state as needed
-  builder.addCase(uploadPackageDocument.fulfilled, (state: SetPackageState, action) => {
-    console.log('uploadPackageFile.fulfilled:action ', action);
-    const { meta } = action;
-    removeUploadingFileFromState(state, meta.arg, meta.requestId);
-  });
-  builder.addCase(uploadPackageDocument.rejected, (state: SetPackageState, action) => {
-    console.log('uploadPackageFile.rejected:action ', action);
-    const { meta } = action;
-    removeUploadingFileFromState(state, meta.arg, meta.requestId);
-  });
+  builder.addCase(
+    uploadPackageDocument.fulfilled,
+    (state: SetPackageState, action) => {
+      console.log('uploadPackageFile.fulfilled:action ', action);
+      const { meta } = action;
+      // removeUploadingFileFromState(state, meta.arg, meta.requestId);
+    }
+  );
+  builder.addCase(
+    uploadPackageDocument.rejected,
+    (state: SetPackageState, action) => {
+      console.log('uploadPackageFile.rejected:action ', action);
+      const { meta } = action;
+      // removeUploadingFileFromState(state, meta.arg, meta.requestId);
+    }
+  );
 };
 
-const removeUploadingFileFromState = (state: SetPackageState, serializedFile: SerializedFile, uploadId: string) => {
+const removeUploadingFileFromState = (
+  state: SetPackageState,
+  serializedFile: SerializedFileUpload,
+  uploadId: string
+) => {
   delete state.documents.uploadingFiles[uploadId];
   cleanSerializedFile(serializedFile);
 };
 
-const getDirectoryTree = (directoryTree: DirectoryTree, directoryTreeId: string): DirectoryTree | undefined => {
+const getDirectoryTree = (
+  directoryTree: DirectoryTree,
+  directoryTreeId: string
+): DirectoryTree | undefined => {
   if (directoryTree.id === directoryTreeId) {
     return directoryTree;
   }
@@ -118,6 +119,11 @@ const getDirectoryTree = (directoryTree: DirectoryTree, directoryTreeId: string)
   });
 };
 
-const removeDirectoryTree = (directoryTree: DirectoryTree, directoryTreeId: string) => {
-  directoryTree.children = directoryTree.children.filter(dt => dt.id !== directoryTreeId);
+const removeDirectoryTree = (
+  directoryTree: DirectoryTree,
+  directoryTreeId: string
+) => {
+  directoryTree.children = directoryTree.children.filter(
+    dt => dt.id !== directoryTreeId
+  );
 };
