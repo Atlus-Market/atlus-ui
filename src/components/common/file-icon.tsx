23d6d5d252659a8ad82cd9549337a29f8b3f76a1
Sergio Milici <sergio.milici@gmail.com>
2023-08-18 16:25:19 -0300

    wip: Add image file type

--- src/components/common/file-icon.tsx
+++ src/components/common/file-icon.tsx
@@ -9,8 +9,15 @@ const supportedFileExtensions = [
   'doc', 'docx', 'pdf', 'xls', 'xlsx', 'ppt', 'pptx', 'zip'
 ];
 
+const imagesExtensions = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'svg'];
+
 export const FileIcon = ({ extension, size = 40 }: FileIconProps) => {
-  const e = supportedFileExtensions.includes(extension) ? extension : 'pdf';
+  const e =
+    supportedFileExtensions.includes(extension.toLowerCase()) ?
+      extension :
+      imagesExtensions.includes(extension.toLowerCase()) ?
+        'image' :
+        'pdf';
   const iconName = `files/${e}.svg`;
   return (
     <Icon name={iconName} size={size} />
