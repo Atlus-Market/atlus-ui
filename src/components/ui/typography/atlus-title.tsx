53eb0c4edc42bfb41ec95607fccbd25f1b2bbb6e
Brian Hoffman <hoffmabc@users.noreply.github.com>
2023-08-21 15:28:45 -0400

    Font Fixes

--- src/components/ui/typography/atlus-title.tsx
+++ src/components/ui/typography/atlus-title.tsx
@@ -1,11 +1,17 @@
+import { Geologica } from 'next/font/google';
+
 interface TitleProps {
   text: string;
   className?: string;
 }
 
+const geologica = Geologica({ subsets: ['latin'], fallback: ['sans-serif'] });
+
 export const AtlusTitle = ({ text, className }: TitleProps) => {
   return (
-    <h3 className={`${className} text-[18px] md:text-[28px] leading-[35px] text-soft-black`}>
+    <h3
+      className={`${geologica.className} ${className} text-[18px] md:text-[28px] leading-[35px] text-soft-black`}
+    >
       {text}
     </h3>
   );
