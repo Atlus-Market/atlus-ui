dfe1951e7ff8fd81d5511af9627ecdace0074088
Sergio Milici <sergio.milici@gmail.com>
2023-08-22 13:05:03 -0300

    wip: Add next button on package detail page

--- src/app/set-package/(pages)/package-details/page.tsx
+++ src/app/set-package/(pages)/package-details/page.tsx
@@ -4,6 +4,9 @@ import {
   getInterestAreas,
   GetInterestAreasResponse
 } from '@/api/interest-areas/get-interest-areas';
+import { AtlusButton } from '@/components/ui/button/atlus-button';
+import Link from 'next/link';
+import { SetPackageDocuments } from '@/constants/routes';
 
 export default async function PackageDetailsPage() {
   const interestAreasResponse: GetInterestAreasResponse = await getInterestAreas();
@@ -11,6 +14,11 @@ export default async function PackageDetailsPage() {
     <div>
       <AtlusTitle text='Package Details' className='!font-normal !text-2xl mb-6' />
       <PackageDetails interestArea={interestAreasResponse.interestArea} />
+      <div className='flex justify-end mt-[60px]'>
+        <Link href={SetPackageDocuments}>
+          <AtlusButton size='medium'>Next</AtlusButton>
+        </Link>
+      </div>
     </div>
   );
 }
