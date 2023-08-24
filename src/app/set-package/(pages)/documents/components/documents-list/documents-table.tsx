bae7398f40e0ca8aa97b1981ec33f3d221b65778
Sergio Milici <sergio.milici@gmail.com>
2023-08-24 18:36:59 -0300

    wip: Show documents size and uploaded date

--- src/app/set-package/(pages)/documents/components/documents-list/documents-table.tsx
+++ src/app/set-package/(pages)/documents/components/documents-list/documents-table.tsx
@@ -4,12 +4,8 @@ import { Fragment } from 'react';
 import { parseGMTDate } from '@/utils/date';
 import format from 'date-fns/format';
 import { FileName } from '@/components/common/file/file-name';
-import {
-  DocumentVisibility
-} from '@/app/set-package/(pages)/documents/components/documents-list/document-visibility';
-import {
-  RemoveDocumentButton
-} from '@/app/set-package/(pages)/documents/components/documents-list/remove-document-button';
+import { DocumentVisibility } from '@/app/set-package/(pages)/documents/components/documents-list/document-visibility';
+import { RemoveDocumentButton } from '@/app/set-package/(pages)/documents/components/documents-list/remove-document-button';
 
 interface DocumentsTableProps {
   dataroom: Dataroom;
@@ -21,35 +17,46 @@ const formatUploadedDate = (gmtString: string): string => {
   return uploadedDate ? format(uploadedDate, 'LLL dd, yyyy') : '-';
 };
 
-export const DocumentsTable = ({ dataroom, onDocumentChanged }: DocumentsTableProps) => {
-  const documents = dataroom.directoryTree.children.filter(directory => directory.type === 'file');
+export const DocumentsTable = ({
+  dataroom,
+  onDocumentChanged,
+}: DocumentsTableProps) => {
+  const documents = dataroom.directoryTree.children.filter(
+    directory => directory.type === 'file'
+  );
 
   return (
-    <div className='documents-grid-scroller'>
-      <div className='documents-grid'>
-        <div className='grid-header grid-first-header-col'>Name</div>
-        <div className='grid-header'>Date uploaded</div>
-        <div className='grid-header'>Visibility</div>
-        <div className='grid-header grid-last-header-col'>{' '}</div>
-        {dataroom.map(document => (
-          <Fragment key={document.name}>
-            <div className='grid-entry'>
+    <div className="documents-grid-scroller">
+      <div className="documents-grid">
+        <div className="grid-header grid-first-header-col">Name</div>
+        <div className="grid-header">Date uploaded</div>
+        <div className="grid-header">Visibility</div>
+        <div className="grid-header grid-last-header-col"> </div>
+        {documents.map(document => (
+          <Fragment key={document.id}>
+            <div className="grid-entry">
               <FileName fileName={document.name} fileSize={document.size} />
             </div>
-            <div className='grid-entry'>
-              <span
-                className='text-soft-black text-sm'>{formatUploadedDate(document.dateUploaded)}</span>
+            <div className="grid-entry">
+              <span className="text-soft-black text-sm">
+                {formatUploadedDate(document.dateUploaded)}
+              </span>
             </div>
-            <div className='grid-entry'>
+            <div className="grid-entry">
               <DocumentVisibility
                 dataroomId={dataroom.directoryTree.name}
                 documentId={document.id}
                 isPrivate={document.private}
-                onDocumentVisibilityChanged={() => onDocumentChanged?.(document.id)}
+                onDocumentVisibilityChanged={() =>
+                  onDocumentChanged?.(document.id)
+                }
               />
             </div>
-            <div className='grid-entry flex'>
-              <RemoveDocumentButton documentId={document.id} />
+            <div className="grid-entry flex">
+              <RemoveDocumentButton
+                dataroomId={dataroom.id}
+                documentId={document.id}
+              />
             </div>
           </Fragment>
         ))}
