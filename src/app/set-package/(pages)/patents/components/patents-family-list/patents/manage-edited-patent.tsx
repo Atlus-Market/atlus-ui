64a5848adb6eeb831922b76b3b258e5ae092430c
Sergio Milici <sergio.milici@gmail.com>
2023-08-30 09:22:26 -0300

    wip: Add Confirmation Alert to remove a patent

--- src/app/set-package/(pages)/patents/components/patents-family-list/patents/manage-edited-patent.tsx
+++ src/app/set-package/(pages)/patents/components/patents-family-list/patents/manage-edited-patent.tsx
@@ -7,14 +7,19 @@ import {
 } from '@/app/set-package/(pages)/patents/components/add-patents/select-patents/use-table-group-patents-by-family';
 import { useAppDispatch } from '@/redux/hooks';
 import { deletePatent, showEditPatentModal } from '@/redux/features/set-package/set-package';
+import { AtlusAlertModal } from '@/components/ui/modal/confirmation/atlus-alert-modal';
+import { useState } from 'react';
 
 interface ManageEditedPatentProps {
   patent: PatentModel;
 }
 
 export const ManageEditedPatent = ({ patent }: ManageEditedPatentProps) => {
+  const [isShowingAlertModal, setIsShowingAlertModal] = useState<boolean>(false);
   const isCreatedManually = patent.familyId === NO_FAMILY_GROUP_ID;
   const dispatch = useAppDispatch();
+
+  const hideAlertModal = () => setIsShowingAlertModal(false);
 
   return (
     <div className='flex items-start gap-2'>
@@ -23,9 +28,25 @@ export const ManageEditedPatent = ({ patent }: ManageEditedPatentProps) => {
         <HiPencil className='text-middle-grey' />
       </button>}
       <button
-        onClick={() => dispatch(deletePatent({ patentId: patent.publicationNumber }))}>
+        onClick={() => setIsShowingAlertModal(true)}>
         <HiTrash className='text-middle-grey' />
       </button>
+      <AtlusAlertModal
+        isOpen={isShowingAlertModal}
+        title='Remove patent?'
+        text='Remove this patent from your package.'
+        mainButton={{
+          text: 'Remove',
+          onClick: () => {
+            hideAlertModal();
+            dispatch(deletePatent({ patentId: patent.publicationNumber }));
+          }
+        }}
+        secondaryButton={{
+          text: 'Cancel',
+          onClick: hideAlertModal
+        }}
+      />
     </div>
   );
 };
