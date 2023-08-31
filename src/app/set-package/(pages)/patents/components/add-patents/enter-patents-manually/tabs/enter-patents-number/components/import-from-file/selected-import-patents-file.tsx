cd7569e2e9c7763460cd0d2fac613f99e4735348
Sergio Milici <sergio.milici@gmail.com>
2023-08-31 15:05:15 -0300

    wip: Implement file container

--- src/app/set-package/(pages)/patents/components/add-patents/enter-patents-manually/tabs/enter-patents-number/components/import-from-file/selected-import-patents-file.tsx
+++ src/app/set-package/(pages)/patents/components/add-patents/enter-patents-manually/tabs/enter-patents-number/components/import-from-file/selected-import-patents-file.tsx
@@ -1,5 +1,7 @@
 import { selectImportPatentsSerializedFile } from '@/redux/features/set-package/selectors/add-patents.selectors';
 import { useAppSelector } from '@/redux/hooks';
+import { FileName } from '@/components/common/file/file-name';
+import { FileContainer } from '@/components/common/file/file-container';
 
 export const SelectedImportPatentsFile = () => {
   const selectedFile = useAppSelector(selectImportPatentsSerializedFile);
@@ -7,5 +9,11 @@ export const SelectedImportPatentsFile = () => {
   if (!selectedFile) {
     return null;
   }
-  return <div>{selectedFile.name}</div>;
+  return (
+    <FileContainer
+      file={
+        <FileName fileName={selectedFile.name} fileSize={selectedFile.size} />
+      }
+    />
+  );
 };
