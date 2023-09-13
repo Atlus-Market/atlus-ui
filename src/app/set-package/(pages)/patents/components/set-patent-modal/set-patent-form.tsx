5812dc8983b240a3e86c0250adfd89605f84b3fe
Sergio Milici <sergio.milici@gmail.com>
2023-09-13 18:31:13 -0300

    wip: Fix Date on custom patents

--- src/app/set-package/(pages)/patents/components/set-patent-modal/set-patent-form.tsx
+++ src/app/set-package/(pages)/patents/components/set-patent-modal/set-patent-form.tsx
@@ -3,11 +3,17 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { AtlusForm } from '@/components/ui/form/atlus-form';
 import { object, ObjectSchema, string } from 'yup';
 import { RequiredField } from '@/constants/form';
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { Patent } from '@/models/patent';
-import { plainDateValidator } from '@/components/ui/form/validators/plain-date-validator';
+import {
+  parseDateString,
+  plainDateFormat,
+  plainDateValidator,
+} from '@/components/ui/form/validators/plain-date-validator';
 import { CustomPatentPayload } from '@/api/package/create-package';
 import { mapCustomPatentToPatent, mapPatentToCustomPatentPayload } from '@/utils/patents';
+import format from 'date-fns/format';
+import { parseGMTDate } from '@/utils/date';
 
 const schema: ObjectSchema<CustomPatentPayload> = object({
   title: string().trim().required(RequiredField),
@@ -15,11 +21,12 @@ const schema: ObjectSchema<CustomPatentPayload> = object({
   applicationNumber: string().trim().required(RequiredField),
   patentNumber: string().default('').required(RequiredField),
   assignee: string().required(RequiredField),
-  applicationDate: string().trim().required(RequiredField).test(plainDateValidator),
+  applicationDate: string()
+    .trim()
+    .required(RequiredField)
+    .test(plainDateValidator)
+    .transform(parseDateString),
 });
-
-// @ts-ignore
-window.patentSchema = schema;
 
 interface SetPatentForm {
   children: ReactNode;
@@ -28,18 +35,28 @@ interface SetPatentForm {
 }
 
 export const SetPatentForm = ({ initialValues, children, onSubmit }: SetPatentForm) => {
-  const initialCustomPatent = initialValues && mapPatentToCustomPatentPayload(initialValues);
+  const initialCustomPatentValues = useMemo(() => {
+    const customPatentPayload = initialValues && mapPatentToCustomPatentPayload(initialValues);
+    if (customPatentPayload?.applicationDate) {
+      const applicationDate = parseGMTDate(customPatentPayload.applicationDate);
+      if (applicationDate) {
+        customPatentPayload.applicationDate = format(applicationDate, plainDateFormat);
+      }
+    }
+    return customPatentPayload;
+  }, []);
+
   const formProps = useAtlusForm<CustomPatentPayload>({
     formOptions: {
       resolver: yupResolver(schema),
-      defaultValues: initialCustomPatent && initialCustomPatent,
+      defaultValues: initialCustomPatentValues && initialCustomPatentValues,
     },
   });
 
   const {
     formState: { errors },
   } = formProps;
-  console.log('SetPatent initialValues; ', initialCustomPatent);
+  console.log('SetPatent initialValues; ', initialCustomPatentValues);
   console.log('SetPatent FormError; ', errors);
 
   const onFormSubmit = (customPatent: CustomPatentPayload) => {
--- src/components/ui/form/validators/plain-date-validator.ts
+++ src/components/ui/form/validators/plain-date-validator.ts
@@ -1,7 +1,8 @@
 import { Validator } from '@/components/ui/form/validators/validator';
 import parse from 'date-fns/parse';
+import { parseGMTDate } from '@/utils/date';
 
-export const plainDateFormat = 'MM/DD/YYYY';
+export const plainDateFormat = 'MM/dd/yyyy';
 
 export const plainDateValidator: Validator<string> = {
   name: 'plain-date-validator',
@@ -13,8 +14,15 @@ const isValidDate = (date: string): boolean => {
   if (!date) {
     return false;
   }
-  const parsedDate = parse(date, 'MM/dd/yyyy', new Date());
-  console.log('parsedDate: ', parsedDate);
-  console.log('parsedDate.getTime: ', parsedDate.getTime());
+  const parsedDate = parseGMTDate(date);
+  console.log('isValidDate: ', parsedDate);
+  if (!parsedDate) {
+    return false;
+  }
   return !isNaN(parsedDate.getTime());
+};
+
+export const parseDateString = (value: any, originalValue: string) => {
+  const parsedDate = parse(originalValue, plainDateFormat, new Date());
+  return parsedDate.toUTCString();
 };
