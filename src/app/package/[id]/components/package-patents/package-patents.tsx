344e24178d5e4e27ab50e5f9e4098e6e1adfaf26
Sergio Milici <sergio.milici@gmail.com>
2023-09-18 15:22:40 -0300

    wip: fix table border-bottom

--- src/app/package/[id]/components/package-patents/package-patents.tsx
+++ src/app/package/[id]/components/package-patents/package-patents.tsx
@@ -4,6 +4,7 @@ import { pluralize } from '@/utils/words';
 import { AtlusTitle } from '@/components/ui/typography/atlus-title';
 import { PackageDivider } from '@/app/package/[id]/components/package-divider';
 import { PackagePatentsTableCompact } from '@/app/package/[id]/components/package-patents/components/package-patents-table-compact';
+import { AtlusButton } from '@/components/ui/button/atlus-button';
 
 interface PackagePatentsTableProps {
   atlusPackage: Package;
@@ -21,6 +22,9 @@ export const PackagePatents = ({ atlusPackage }: PackagePatentsTableProps) => {
         className="!font-normal !text-base md:!text-xl text-black mb-4 md:mb-6"
       />
       <PackagePatentsTableCompact patents={allPatents} />
+      <AtlusButton variant="outline" className="mt-4 md:mt-6">
+        See all patents
+      </AtlusButton>
       <PackageDivider />
       {/*<PackagePatentsTable patents={allPatents} type="full" />*/}
     </div>
