dac79b9bbcd9128232f1e99943c9525403a4182e
Sergio Milici <sergio.milici@gmail.com>
2023-09-20 12:05:00 -0300

    Fix persist package with visibility fixes

--- src/redux/features/set-package/thunks/set-package.thunk.ts
+++ src/redux/features/set-package/thunks/set-package.thunk.ts
@@ -15,9 +15,11 @@ import {
 import { updatePackage } from '@/api/package/update-package';
 import { NO_FAMILY_GROUP_ID } from '@/app/set-package/(pages)/patents/components/add-patents/select-patents/use-table-group-patents-by-family';
 import { getPatentId, mapPatentToCustomPatentPayload } from '@/utils/patents';
+import { IPackageDetailsForm } from '@/app/set-package/(pages)/package-details/package-details-form';
+import { Visibility } from '@/components/common/dropdown/visibility-options';
 
 export interface PersistPackageResult {
-  package: Package;
+  package?: Package;
   createdPackage: boolean;
 }
 
@@ -27,39 +29,20 @@ export const persistPackage = createAsyncThunk<PersistPackageResult, void, { sta
     try {
       const { getState } = thunkAPI;
       const activePackage = selectPackage(getState());
-
       const allPatents: Patent[] = selectPackagePatents(getState());
-      const patents = allPatents.filter(p => p.familyId !== NO_FAMILY_GROUP_ID);
-      const customPatents = allPatents.filter(p => p.familyId === NO_FAMILY_GROUP_ID);
-      const patentsIds = patents.map(p => getPatentId(p));
-
-      console.log('patents: ', patents);
-      console.log('customPatents: ', customPatents);
-      console.log('getPatentsIds: ', patentsIds);
-
       const packageDetails = selectPackageDetailsFormValues(getState());
       console.log('packageDetails: ', packageDetails);
 
-      const payload: CreatePackageRequestPayload = {
-        ...packageDetails,
-        keywords: packageDetails.keywords.join(','),
-        industryIds: packageDetails.industryIds.map(id => parseInt(id, 10)),
-        visibility: packageDetails.visibility ? 1 : 0,
-        patents: patentsIds,
-        customPatents: getCustomPatents(customPatents),
-      };
+      const payload: CreatePackageRequestPayload = packageDetailsFormToPersistPackagePayload(
+        packageDetails,
+        allPatents
+      );
 
       if (activePackage) {
         const res = await updatePackage(activePackage.id, payload);
         console.log('UPDATE package Response: ', res);
         return {
           createdPackage: false,
-          package: {
-            ...payload,
-            ...activePackage,
-            patents,
-            customPatents,
-          },
         };
       } else {
         const res = await createPackage(payload);
@@ -78,4 +61,26 @@ export const persistPackage = createAsyncThunk<PersistPackageResult, void, { sta
 
 const getCustomPatents = (patents: Patent[]): CustomPatentPayload[] => {
   return patents.filter(p => p.familyId === NO_FAMILY_GROUP_ID).map(mapPatentToCustomPatentPayload);
+};
+
+const packageDetailsFormToPersistPackagePayload = (
+  packageDetails: IPackageDetailsForm,
+  allPatents: Patent[]
+): CreatePackageRequestPayload => {
+  const patents = allPatents.filter(p => p.familyId !== NO_FAMILY_GROUP_ID);
+  const customPatents = allPatents.filter(p => p.familyId === NO_FAMILY_GROUP_ID);
+  const patentsIds = patents.map(p => getPatentId(p));
+
+  console.log('patents: ', patents);
+  console.log('customPatents: ', customPatents);
+  console.log('getPatentsIds: ', patentsIds);
+
+  return {
+    ...packageDetails,
+    keywords: packageDetails.keywords.join(','),
+    industryIds: packageDetails.industryIds.map(id => parseInt(id, 10)),
+    visibility: packageDetails.visibility === Visibility.Public ? 1 : 0,
+    patents: patentsIds,
+    customPatents: getCustomPatents(customPatents),
+  };
 };
