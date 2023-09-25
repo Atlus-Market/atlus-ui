2efa0ee3975ceb03dd20b7dc10fd62c30ab46d22
Sergio Milici <sergio.milici@gmail.com>
2023-09-25 15:05:48 -0300

    wip: patents table

--- src/app/package/[id]/components/package-patents/components/package-patents-reduced-table.tsx
+++ src/app/package/[id]/components/package-patents/components/package-patents-reduced-table.tsx
@@ -1,7 +1,6 @@
 import { Patent } from '@/models/patent';
 import { getPatentId, groupPatentsByFamily } from '@/utils/patents';
 import { PatentsInFamilyLink } from '@/app/package/[id]/components/package-patents/components/patents-in-family-link';
-import { Fragment } from 'react';
 import { TableHeaderTitle } from '@/app/package/[id]/components/package-patents/components/table-header-title';
 import { PackageTableApplicationDate } from '@/app/package/[id]/components/package-patents/components/package-table-application-date';
 import { PackageTablePatentId } from '@/app/package/[id]/components/package-patents/components/package-table-patent-id';
@@ -11,60 +10,22 @@ import { PackageTableCell } from '@/app/package/[id]/components/package-patents/
 import clsx from 'clsx';
 import { ViewPackagePatentsProvider } from '@/app/package/[id]/components/package-patents/components/view-package-patents/view-package-patents-provider';
 import { ViewPackagePatentsModal } from '@/app/package/[id]/components/package-patents/components/view-package-patents/view-package-patents-modal';
+import {
+  gridBorderStyles,
+  PackagePatentsTable,
+} from '@/app/package/[id]/components/package-patents/components/package-patents-table';
 
 interface PackagePatentsTableCompactProps {
   patents: Patent[];
 }
 
-const gridBorderStyles = clsx(
-  'border border-light-grey-2 overflow-hidden',
-  'rounded-tl-lg rounded-tr-lg md:rounded-tl-xl md:rounded-tr-xl',
-  'rounded-br-xl rounded-bl-xl'
-);
-
-export const PackagePatentsTableCompact = ({ patents }: PackagePatentsTableCompactProps) => {
+export const PackagePatentsReducedTable = ({ patents }: PackagePatentsTableCompactProps) => {
   const familyPatents = groupPatentsByFamily(patents);
   const patentsGroups = Object.values(familyPatents);
   return (
     <div>
       <ViewPackagePatentsProvider>
-        <div className="hidden md:block">
-          <div className={clsx('grid grid-cols-4', gridBorderStyles)}>
-            <PackageTableHeader className="col-span-2">
-              <TableHeaderTitle title="Title" />
-            </PackageTableHeader>
-            <PackageTableHeader>
-              <TableHeaderTitle title="Publication/Patent no." />
-            </PackageTableHeader>
-            <PackageTableHeader>
-              <TableHeaderTitle title="Filling date" />
-            </PackageTableHeader>
-            {patentsGroups.map((patents, index) => {
-              const patent = patents[0];
-              const patentId = getPatentId(patent);
-              const isLastRow = patentsGroups.length - 1 === index;
-              return (
-                <Fragment key={patentId}>
-                  <PackageTableCell className={clsx('col-span-2', { '!border-b-0': isLastRow })}>
-                    <PackageTablePatentTitle title={patent.title} />
-                    {patents.length > 1 && (
-                      <div className="mt-3">
-                        <PatentsInFamilyLink patents={patents} />
-                      </div>
-                    )}
-                  </PackageTableCell>
-                  <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
-                    <PackageTablePatentId patentId={patentId} />
-                  </PackageTableCell>
-                  <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
-                    <PackageTableApplicationDate gmtDate={patent.applicationDate} />
-                  </PackageTableCell>
-                </Fragment>
-              );
-            })}
-          </div>
-        </div>
-
+        <PackagePatentsTable patents={patents} type="compact" />
         <div className="md:hidden">
           {patentsGroups.map((patents, index) => {
             const patent = patents[0];
--- src/app/package/[id]/components/package-patents/components/package-patents-table.tsx
+++ src/app/package/[id]/components/package-patents/components/package-patents-table.tsx
@@ -1,5 +1,14 @@
 import { Patent } from '@/models/patent';
 import clsx from 'clsx';
+import { PackageTableHeader } from '@/app/package/[id]/components/package-patents/components/package-table-header';
+import { TableHeaderTitle } from '@/app/package/[id]/components/package-patents/components/table-header-title';
+import { getPatentId, getPatentReadableAssignees, groupPatentsByFamily } from '@/utils/patents';
+import { Fragment } from 'react';
+import { PackageTableCell } from '@/app/package/[id]/components/package-patents/components/package-table-cell';
+import { PackageTablePatentTitle } from '@/app/package/[id]/components/package-patents/components/package-table-patent-title';
+import { PatentsInFamilyLink } from '@/app/package/[id]/components/package-patents/components/patents-in-family-link';
+import { PackageTablePatentId } from '@/app/package/[id]/components/package-patents/components/package-table-patent-id';
+import { PackageTableApplicationDate } from '@/app/package/[id]/components/package-patents/components/package-table-application-date';
 
 export type PackagePatentsTableType = 'compact' | 'full';
 
@@ -8,45 +17,85 @@ interface PackagePatentsTableProps {
   type: PackagePatentsTableType;
 }
 
+export const gridBorderStyles = clsx(
+  'border border-light-grey-2 overflow-hidden',
+  'rounded-tl-lg rounded-tr-lg md:rounded-tl-xl md:rounded-tr-xl',
+  'rounded-br-xl rounded-bl-xl'
+);
+
 export const PackagePatentsTable = ({ patents, type }: PackagePatentsTableProps) => {
+  const familyPatents = groupPatentsByFamily(patents);
+  const patentsGroups = Object.values(familyPatents);
   const isCompact = type === 'compact';
   const isFull = type === 'full';
+
   return (
-    <div
-      className={clsx('grid', {
-        'grid-cols-2 md:grid-cols-3': isCompact,
-        'grid-cols-6': isFull,
-      })}
-    >
+    <div className="hidden md:block">
       <div
-        className={clsx('bg-light-grey-2', {
-          'col-span-2 md:col-span-1': isCompact,
-        })}
+        className={clsx(
+          'grid',
+          {
+            'grid-cols-4': isCompact,
+            'grid-cols-6': isFull,
+          },
+          gridBorderStyles
+        )}
       >
-        1
-      </div>
-      <div>2</div>
-      <div>3</div>
-      <div
-        className={clsx({
-          'bg-light-grey hidden': isCompact,
+        <PackageTableHeader className="col-span-2">
+          <TableHeaderTitle title="Title" />
+        </PackageTableHeader>
+        <PackageTableHeader>
+          <TableHeaderTitle title="Publication/Patent no." />
+        </PackageTableHeader>
+        {isFull && (
+          <PackageTableHeader>
+            <TableHeaderTitle title="Assignee" />
+          </PackageTableHeader>
+        )}
+        {isFull && (
+          <PackageTableHeader>
+            <TableHeaderTitle title="Application no." />
+          </PackageTableHeader>
+        )}
+        <PackageTableHeader>
+          <TableHeaderTitle title="Filling date" />
+        </PackageTableHeader>
+
+        {patentsGroups.map((patentsGroup, index) => {
+          const patentsToRender = isCompact ? [patentsGroup[0]] : patentsGroup;
+          return patentsToRender.map(patent => {
+            const patentId = getPatentId(patent);
+            const isLastRow = patentsGroups.length - 1 === index;
+            return (
+              <Fragment key={patentId}>
+                <PackageTableCell className={clsx('col-span-2', { '!border-b-0': isLastRow })}>
+                  <PackageTablePatentTitle title={patent.title} />
+                  {isCompact && patentsGroup.length > 1 && (
+                    <div className="mt-3">
+                      <PatentsInFamilyLink patents={patentsGroup} />
+                    </div>
+                  )}
+                </PackageTableCell>
+                <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
+                  <PackageTablePatentId patentId={patentId} />
+                </PackageTableCell>
+                {isFull && (
+                  <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
+                    {getPatentReadableAssignees(patent)}
+                  </PackageTableCell>
+                )}
+                {isFull && (
+                  <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
+                    {patent.applicationNumber}
+                  </PackageTableCell>
+                )}
+                <PackageTableCell className={clsx({ '!border-b-0': isLastRow })}>
+                  <PackageTableApplicationDate gmtDate={patent.applicationDate} />
+                </PackageTableCell>
+              </Fragment>
+            );
+          });
         })}
-      >
-        4
-      </div>
-      <div
-        className={clsx({
-          'bg-light-grey hidden': isCompact,
-        })}
-      >
-        5
-      </div>
-      <div
-        className={clsx({
-          'bg-light-grey hidden': isCompact,
-        })}
-      >
-        6
       </div>
     </div>
   );
