3ae6ab0b9aa95fc89fae1f5e55cd4734911447fa
Sergio Milici <sergio.milici@gmail.com>
2023-09-26 15:48:56 -0300

    wip: Add patent image with zoom

--- src/app/set-package/(pages)/patents/components/patents-family-list/patents/patent.tsx
+++ src/app/set-package/(pages)/patents/components/patents-family-list/patents/patent.tsx
@@ -3,6 +3,7 @@ import DocumentSVG from '@/public/assets/images/document.svg';
 import Image from 'next/image';
 import { ManageEditedPatent } from '@/app/set-package/(pages)/patents/components/patents-family-list/patents/manage-edited-patent';
 import { getPatentId, getPatentReadableAssignees } from '@/utils/patents';
+import { AtlusImageZoom } from '@/components/common/atlus-image-zoom';
 
 interface PatentProps {
   patent: PatentModel;
@@ -13,13 +14,29 @@ export const Patent = ({ patent }: PatentProps) => {
     <div className="flex item-start justify-between px-6 py-4">
       <div className="flex item-start justify-between">
         <div className="inline-block mr-[43px] flex-shrink-0">
-          <Image
-            src={DocumentSVG}
-            alt="Document"
-            width={126}
-            height={126}
-            className="h-full max-h-[126px]"
-          />
+          {patent.thumbnail ? (
+            <AtlusImageZoom>
+              <img
+                src={patent.thumbnail}
+                alt="patent thumbnail"
+                width="auto"
+                height={126}
+                className="!h-[126px]"
+              />
+            </AtlusImageZoom>
+          ) : (
+            <Image
+              src={DocumentSVG}
+              alt="Document"
+              width={126}
+              height={126}
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}
+              className="h-full max-h-[126px]"
+            />
+          )}
         </div>
         <div>
           <div className="mb-1">
