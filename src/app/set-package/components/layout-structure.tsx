bd05ff4ac6ce7786fa50d918209c6dc82d768804
Sergio Milici <sergio.milici@gmail.com>
2023-10-02 15:49:54 -0300

    wip: make header sticky and scrollable with side bar

--- src/app/set-package/components/layout-structure.tsx
+++ src/app/set-package/components/layout-structure.tsx
@@ -9,8 +9,8 @@ interface LayoutStructureProps {
 export const LayoutStructure = ({ sideBarChildren, children }: LayoutStructureProps) => {
   return (
     <div className="grid grid-cols-[240px,minmax(400px,1200px)]">
-      <div>{sideBarChildren}</div>
-      <PageWrapper>{children}</PageWrapper>
+      <div className="w-[240px] fixed">{sideBarChildren}</div>
+      <PageWrapper className="col-start-2">{children}</PageWrapper>
     </div>
   );
 };
