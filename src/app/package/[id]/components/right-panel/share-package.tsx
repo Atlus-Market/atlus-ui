c4b0c58b735791e5e666f9dc675ab0cb0b79d16a
Sergio Milici <sergio.milici@gmail.com>
2023-10-04 11:16:06 -0300

    wip: hide share modal auto show

--- src/app/package/[id]/components/right-panel/share-package.tsx
+++ src/app/package/[id]/components/right-panel/share-package.tsx
@@ -8,7 +8,7 @@ import { useToggleState } from '@/hooks/use-toggle-state';
 interface SharePackageProps {}
 
 export const SharePackage = ({}: SharePackageProps) => {
-  const { isOn: isModalOpen, setOn: setIsModalOpen, setOff: closeModal } = useToggleState(true);
+  const { isOn: isModalOpen, setOn: setIsModalOpen, setOff: closeModal } = useToggleState(false);
 
   return (
     <div>
