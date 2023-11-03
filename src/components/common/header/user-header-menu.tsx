7bd191a15dbd73c31e5fbfeea449f556eefc6e63
Sergio Milici <sergio.milici@gmail.com>
2023-11-03 17:27:43 -0300

    wip: Update links to use Link

--- src/components/common/header/user-header-menu.tsx
+++ src/components/common/header/user-header-menu.tsx
@@ -4,19 +4,16 @@ import { AtlusAvatar } from '@/components/common/avatar/atlus-avatar';
 import { AtlusMenuItem } from '@/components/ui/menu/atlus-menu-item';
 import { AtlusMenuDivider } from '@/components/ui/menu/atlus-menu-divider';
 import { AtlusMenu } from '@/components/ui/menu/atlus-menu';
-import { useLogout } from '@/app/(auth)/session/use-logout';
-import { useRouter } from 'next/navigation';
-import { DashboardRoute } from '@/constants/routes';
+import { DashboardRoute, LogoutRoute } from '@/constants/routes';
 import { useAtlusUser } from '@/app/(auth)/session/use-atlus-user';
 import { useHasAtlusSession } from '@/app/(auth)/session/use-has-atlus-session';
+import Link from 'next/link';
 
 interface UserHeaderMenuProps {}
 
 export const UserHeaderMenu = ({}: UserHeaderMenuProps) => {
   const { data: user } = useAtlusUser();
   const hasAtlusSession = useHasAtlusSession();
-  const logout = useLogout();
-  const router = useRouter();
 
   if (!hasAtlusSession) {
     return null;
@@ -31,10 +28,14 @@ export const UserHeaderMenu = ({}: UserHeaderMenuProps) => {
       }
       menuItems={
         <>
-          <AtlusMenuItem text={'Dashboard'} onClick={() => router.push(DashboardRoute)} />
+          <Link href={DashboardRoute}>
+            <AtlusMenuItem text={'Dashboard'} />
+          </Link>
           <AtlusMenuItem text={'Settings'} />
           <AtlusMenuDivider />
-          <AtlusMenuItem text={'Log out'} onClick={logout} />
+          <Link href={LogoutRoute}>
+            <AtlusMenuItem text={'Log out'} />
+          </Link>
         </>
       }
     />
