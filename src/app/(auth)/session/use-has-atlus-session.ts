05c7ba4df80f683bb6f69be9a609747e12bdfab7
Sergio Milici <sergio.milici@gmail.com>
2023-11-21 17:35:48 -0300

    wip: remove log

--- src/app/(auth)/session/use-has-atlus-session.ts
+++ src/app/(auth)/session/use-has-atlus-session.ts
@@ -3,6 +3,5 @@ import { useAtlusSession } from '@/app/(auth)/session/use-atlus-session';
 export const useHasAtlusSession = (): boolean => {
   const session = useAtlusSession();
   const hasAtlusInvalidSession = !!session.data?.hasAtlusInvalidSession;
-  console.log('hasAtlusInvalidSession: ', hasAtlusInvalidSession);
   return session.status === 'authenticated' && !hasAtlusInvalidSession;
 };
