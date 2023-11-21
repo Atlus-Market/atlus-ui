0d30ecfd222baaa0f3287f58263a7ca4a24f59df
Sergio Milici <sergio.milici@gmail.com>
2023-11-21 17:36:47 -0300

    wip: remove log

--- src/app/(auth)/session/atlus-session-handler.tsx
+++ src/app/(auth)/session/atlus-session-handler.tsx
@@ -28,7 +28,6 @@ export const AtlusSessionHandler = ({ children }: AtlusTokenHandlerProps) => {
   }, [atlusSession.status, session]);
 
   useEffect(() => {
-    console.log(pathname);
     if (pathname === LogoutRoute) {
       return;
     }
