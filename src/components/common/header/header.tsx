10f6f7b51fef2ee5f6e1c0f8bc45e8ba160af489
Sergio Milici <sergio.milici@gmail.com>
2023-12-01 16:24:52 -0300

    wip: Fix header height

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header
       className={clsx(
-        'px-4 py-[10px] md:px-10 md:pt-[22px] md:pb-[21px]',
+        'px-4 md:px-10 py-0 min-h-[60px] md:min-h-[80px]',
         'bg-white',
         'flex items-center justify-between gap-4',
         'min-h-[--atlus-header-height]',
