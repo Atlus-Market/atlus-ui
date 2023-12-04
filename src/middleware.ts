cb0e91b0fdc0db5c7588e5dbfe6a25916ac63e4b
Sergio Milici <sergio.milici@gmail.com>
2023-12-04 12:31:30 -0300

    wip: Add middleware validation for buyer access to broker pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export default async function middlewares(req: NextRequest, event: NextFetchEven
   const logoutPath = '/logout';
   const token = await getToken({ req });
   const accessToken = token?.user?.accessToken;
+  const isBrokerUser = token?.user?.isBroker || false;
   const isAuthenticated = !!token;
   const isTokenExpired = isAuthenticated && accessToken && hasTokenExpired(accessToken);
   const { pathname } = req.nextUrl;
@@ -27,12 +28,18 @@ export default async function middlewares(req: NextRequest, event: NextFetchEven
   const isUnauthenticatedPathname = isUnauthenticatedEndpoint(pathname);
   const canRunOnAllCases = canEndpointRunOnAllCases(pathname);
 
+  // Prevent access to pages that are for users with no auth session
   if (isAuthenticated && isUnauthenticatedPathname && !canRunOnAllCases) {
     // Do not redirect to "/" because the redirect is being done in next.config.js
     return NextResponse.redirect(new URL('/dashboard', req.url));
   } else if (isUnauthenticatedPathname || canRunOnAllCases) {
     // Continue loading the free endpoint
     return NextResponse.next();
+  }
+
+  if (isBrokerOnlyPathname(pathname) && !isBrokerUser) {
+    console.log('User is not broker. Redirecting...');
+    return NextResponse.redirect(new URL('/dashboard', req.url));
   }
 
   // The pathname requires auth and the user is not logged in
@@ -69,4 +76,8 @@ const isUnauthenticatedEndpoint = (pathname: string): boolean => {
 
 export const config = {
   matcher: ['/((?!_next|api/auth).*)(.+)'],
+};
+
+const isBrokerOnlyPathname = (pathname: string): boolean => {
+  return new RegExp(/(^\/set-package\/*).*/).test(pathname);
 };
