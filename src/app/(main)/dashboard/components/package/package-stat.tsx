f5be39ae08274bf9def58d723a1ce7b20c9fed9c
Sergio Milici <sergio.milici@gmail.com>
2023-12-07 12:55:27 -0300

    wip: Add package tooltips

--- src/app/(main)/dashboard/components/package/package-stat.tsx
+++ src/app/(main)/dashboard/components/package/package-stat.tsx
@@ -4,12 +4,18 @@ import { IconBaseProps } from 'react-icons/lib/cjs/iconBase';
 interface PackageStatProps {
   value: number | string;
   icon: JSXElementConstructor<IconBaseProps>;
+  tooltipId: string;
+  tooltipContent: string;
 }
 
-export const PackageStat = ({ value, icon }: PackageStatProps) => {
+export const PackageStat = ({ value, icon, tooltipId, tooltipContent }: PackageStatProps) => {
   const Icon = icon;
   return (
-    <div className="flex items-center gap-1 leading-none">
+    <div
+      className="flex items-center gap-1 leading-none"
+      data-tooltip-id={tooltipId}
+      data-tooltip-content={tooltipContent}
+    >
       <Icon className="text-dark-grey text-base md:text-lg" />
       <span className="text-dark-grey text-xs md:text-sm !leading-none">{value}</span>
     </div>
--- src/app/(main)/dashboard/components/package/package-stats.tsx
+++ src/app/(main)/dashboard/components/package/package-stats.tsx
@@ -2,18 +2,42 @@ import { PackageStat } from '@/app/(main)/dashboard/components/package/package-s
 import { HiEye, HiShare } from 'react-icons/hi2';
 import { HiDownload, HiX } from 'react-icons/hi';
 import { PackageStats as PackageStatsType } from '@/models/package-stats';
+import { AtlusTooltip } from '@/components/ui/tooltip/atlus-tooltip';
+import { useId } from 'react';
 
 interface PackageStatsProps {
   packageStats: PackageStatsType;
 }
 
 export const PackageStats = ({ packageStats }: PackageStatsProps) => {
+  const tooltipId = useId();
   return (
     <div className="flex items-center gap-5 md:gap-8">
-      <PackageStat icon={HiEye} value={packageStats.views} />
-      <PackageStat icon={HiShare} value={packageStats.shares} />
-      <PackageStat icon={HiDownload} value={packageStats.downloads} />
-      <PackageStat icon={HiX} value={packageStats.notInterestedCount} />
+      <AtlusTooltip tooltipId={tooltipId} />
+      <PackageStat
+        icon={HiEye}
+        value={packageStats.views}
+        tooltipId={tooltipId}
+        tooltipContent="Views"
+      />
+      <PackageStat
+        icon={HiShare}
+        value={packageStats.shares}
+        tooltipId={tooltipId}
+        tooltipContent="Shares"
+      />
+      <PackageStat
+        icon={HiDownload}
+        value={packageStats.downloads}
+        tooltipId={tooltipId}
+        tooltipContent="Downloads"
+      />
+      <PackageStat
+        icon={HiX}
+        value={packageStats.notInterestedCount}
+        tooltipId={tooltipId}
+        tooltipContent="Not interested"
+      />
     </div>
   );
 };
